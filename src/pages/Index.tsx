efba2c09454805993c9b7575fd7a086ebaf96f13
Varad Charholikar <103520139+varadc-2304@users.noreply.github.com>
2025-06-13 20:40:19 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -209,7 +209,7 @@ const Index = () => {
             </div>
           </div>
           <div className="mt-6 text-center text-sm opacity-70">
-            <p>© 2025 Yodha Coding Arena. All rights reserved.</p>
+            <p>© 2025 Ikshvaku Innovations. All rights reserved.</p>
           </div>
         </div>
       </footer>
